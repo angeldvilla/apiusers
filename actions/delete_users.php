837ead966db652cb1ed2f2542a03401eff596a44
angeldvilla <escarlata.2003@gmail.com>
2023-05-17 23:33:44 -0500

    API PHP

---
+++ actions/delete_users.php
@@ -0,0 +1,59 @@
+
+<?php
+// Conexión a la base de datos MySQL
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "users_api";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+  die("Error al conectar a la base de datos: " . $conn->connect_error);
+} else {
+  echo "Conexión exitosa a la base de datos";
+}
+?>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $data = json_decode(file_get_contents('php://input'), true);
+
+  $username = $data['username'];
+  $password = password_hash($data['password'], PASSWORD_DEFAULT);
+  $name = $data['name'];
+  $lastname = $data['lastname'];
+  $email = $data['email'];
+  $cellphone = $data['cellphone'];
+
+  $sql = "INSERT INTO users (username, password, name, lastname, email, cellphone) VALUES ('$username', '$password', '$name', '$lastname', '$email', '$cellphone' )";
+  if ($conn->query($sql) === TRUE) {
+    $response = array('status' => 'success');
+  } else {
+    $response = array('status' => 'error', 'message' => $conn->error);
+  }
+
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
+?>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+  $data = json_decode(file_get_contents('php://input'), true);
+
+  $id = $data['id_user'];
+  
+ //CONSULTA PARA BORRAR
+  $sql = "DELETE FROM users WHERE id=$id";
+  if ($conn->query($sql) === TRUE) {
+    $response = array('status' => 'success');
+  } else {
+    $response = array('status' => 'error', 'message' => $conn->error);
+  }
+
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
+?>
---
+++ actions/login.php
@@ -0,0 +1,50 @@
+<?php
+// Conexión a la base de datos MySQL
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "users_api";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+  die("Error al conectar a la base de datos: " . $conn->connect_error);
+}
+
+// Obtiene los datos de la solicitud POST
+$data = json_decode(file_get_contents('php://input'), true);
+$username = $data['username'];
+$password = $data['password'];
+
+// Busca el usuario en la base de datos
+$sql = "SELECT * FROM users WHERE username = '$username'";
+$result = $conn->query($sql);
+
+// Si el usuario no existe, devuelve un error
+if ($result->num_rows == 0) {
+  $response = array('status' => 'error', 'message' => 'Nombre de usuario o contraseña incorrectos');
+  header('Content-Type: application/json');
+  echo json_encode($response);
+  exit;
+}
+
+// Obtiene la fila del usuario
+$row = $result->fetch_assoc();
+
+// Verifica la contraseña del usuario
+if (password_verify($password, $row['password'])) {
+  // Si la contraseña es correcta, devuelve un mensaje de éxito
+  $response = array('status' => 'success', 'message' => 'Inicio de sesión exitoso');
+  header('Content-Type: application/json');
+  echo json_encode($response);
+} else {
+  // Si la contraseña es incorrecta, devuelve un error
+  $response = array('status' => 'error', 'message' => 'Nombre de usuario o contraseña incorrectos');
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
+
+// Cierra la conexión a la base de datos
+$conn->close();
+?>
